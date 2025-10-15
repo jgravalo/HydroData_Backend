61b65439d249f5e82c3f8dbe6257deb12faf278c
isaac diez <isaac.diez@proton.me>
2025-10-15 20:38:22 +0200

    refactor(): created getAllData() method

--- repteTres/src/main/java/hackatonGrupUn/repteTres/controller/DataController.java
+++ repteTres/src/main/java/hackatonGrupUn/repteTres/controller/DataController.java
@@ -1,30 +1,25 @@
 package hackatonGrupUn.repteTres.controller;
 
 import hackatonGrupUn.repteTres.model.Data;
+import hackatonGrupUn.repteTres.repository.DataRepository;
 import hackatonGrupUn.repteTres.service.DataService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
 public class DataController {
 
     private final DataService dataService;
+    private final DataRepository dataRepository;
 
-    public DataController(DataService dataService) {
+    public DataController(DataService dataService, DataRepository dataRepository) {
         this.dataService = dataService;
+        this.dataRepository = dataRepository;
     }
 
     @GetMapping("hydraulic/api/v1/data")
-    public List<Data> getConsumptionData() {
-        try {
-            return dataService.loadWaterConsumptionData("/json/2015_consum_aigua.json");
-        } catch (IOException e) {
-            System.err.println("Error reading JSON file:");
-            e.printStackTrace();
-            return Collections.emptyList();
-        }
+    public List<Data> getAllData() {
+            return dataRepository.findAll();
     }
 }
