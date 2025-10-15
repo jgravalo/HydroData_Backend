3c2842150268865f5595b2d093e26ed0e50ccaee
ToniR90 <romy90.t@gmail.com>
2025-10-15 21:44:14 +0200

    feat: User model implementation

---
+++ repteTres/src/main/java/hackatonGrupUn/repteTres/service/UserService.java
@@ -0,0 +1,32 @@
+package hackatonGrupUn.repteTres.service;
+
+import hackatonGrupUn.repteTres.model.User;
+import hackatonGrupUn.repteTres.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
