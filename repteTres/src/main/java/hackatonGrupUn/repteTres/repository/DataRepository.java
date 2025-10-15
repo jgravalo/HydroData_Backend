8db49626436e5158be59d57b6f6cf80efe052cd6
isaac diez <isaac.diez@proton.me>
2025-10-15 19:35:06 +0200

    feat(repo): created to persist data in H2

---
+++ repteTres/src/main/java/hackatonGrupUn/repteTres/repository/DataRepository.java
@@ -0,0 +1,9 @@
+package hackatonGrupUn.repteTres.repository;
+
+import hackatonGrupUn.repteTres.model.Data;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DataRepository extends JpaRepository<Data, Long> {
+}
