5a4d211538ec801325e47fd8aa6147ecf785b031
acocinas <alcogar79@gmail.com>
2025-10-15 16:02:52 +0200

    Delete repteTres/src/main/java/hackatonGrupUn/repteTres/repository/AnomalyRepository.java

--- repteTres/src/main/java/hackatonGrupUn/repteTres/repository/AnomalyRepository.java
+++
@@ -1,9 +0,0 @@
-package hackatonGrupUn.repteTres.repository;
-
-import hackatonGrupUn.repteTres.model.Anomaly;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnomalyRepository extends JpaRepository<Anomaly, Long> {
-}
