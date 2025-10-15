045b501dcc556bc7d9ec4785c1b93644a68fe927
isaac diez <isaac.diez@proton.me>
2025-10-15 20:37:37 +0200

    refactor(): created getAllData() method

--- repteTres/src/main/java/hackatonGrupUn/repteTres/service/DataService.java
+++ repteTres/src/main/java/hackatonGrupUn/repteTres/service/DataService.java
@@ -6,14 +6,13 @@ import hackatonGrupUn.repteTres.jsonUtils.FeatureWrapper;
 import hackatonGrupUn.repteTres.jsonUtils.Root;
 import hackatonGrupUn.repteTres.repository.DataRepository;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Service; // 👈 Importante
+import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class DataService {
-
 
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final DataRepository dataRepository;
@@ -22,7 +21,7 @@ public class DataService {
         this.dataRepository = dataRepository;
     }
 
-    public List<Data> loadWaterConsumptionData(String jsonFilePath) throws IOException {
+    public List<Data> loadInitialData(String jsonFilePath) throws IOException {
 
         ClassPathResource resource = new ClassPathResource(jsonFilePath);
 
@@ -33,5 +32,9 @@ public class DataService {
                 .collect(Collectors.toList());
 
         return dataRepository.saveAll(dataList);
+    }
+
+    public List<Data> getAllData() {
+        return dataRepository.findAll();
     }
 }
