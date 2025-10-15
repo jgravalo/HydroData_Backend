9522dc837eb5864d8c33085b6a51808374024595
acocinas <alcogar79@gmail.com>
2025-10-15 15:41:18 +0200

    feat: Add AnomalyDto class

--- repteTres/src/main/java/hackatonGrupUn/repteTres/dto/AnomalyDto.java
+++ repteTres/src/main/java/hackatonGrupUn/repteTres/dto/AnomalyDto.java
@@ -1,4 +1,36 @@
 package hackatonGrupUn.repteTres.dto;
 
+import java.time.LocalDateTime;
+
 public class AnomalyDto {
+
+    private String location;
+    private String zone;
+    private String description;
+    private LocalDateTime timestamp;
+
+    public AnomalyDto() {}
+
+    public AnomalyDto(String location, String zone, String description, LocalDateTime timestamp) {
+        this.location = location;
+        this.zone = zone;
+        this.description = description;
+        this.timestamp = timestamp;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
 }
