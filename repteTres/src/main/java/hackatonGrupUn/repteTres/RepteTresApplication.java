4ce4080e393d1c8ab5550d515b9b045e0c9dc8b8
acocinas <alcogar79@gmail.com>
2025-10-15 10:28:44 +0200

    feat: Initial project setup with Maven multi-module architecture

---
+++ repteTres/src/main/java/hackatonGrupUn/repteTres/RepteTresApplication.java
@@ -0,0 +1,13 @@
+package hackatonGrupUn.repteTres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepteTresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepteTresApplication.class, args);
+	}
+
+}
---
+++ repteTres/src/test/java/hackatonGrupUn/repteTres/RepteTresApplicationTests.java
@@ -0,0 +1,13 @@
+package hackatonGrupUn.repteTres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepteTresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
